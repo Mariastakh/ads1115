155a8f07585d84e5664d92b3f1b65bde6ce780b9
Maria Astakhova <32569878+Mariastakh@users.noreply.github.com>
2019-05-18 20:57:12 +0100

    Update ads1115.cpp

--- ads1115.cpp
+++ ads1115.cpp
@@ -1,15 +1,19 @@
-#include <wiringPi.h>
+// Include the wiringPi library, and the ads1115.h script (its included in the wiringPi library)
+#include <wiringPi.h> 
 #include <ads1115.h>
 #include <iostream>
 
 int main(void)
 {
+  // Settup:
   wiringPiSetup();
+  // Listen on 0x48, 70 can be any number
   ads1115Setup(70, 0x48);
 
 
     for(;;)
     {
+       // Read channel 0:
        int ch0 = analogRead(70+0);
        Std::cout << ch0 << std::endl;
     }
